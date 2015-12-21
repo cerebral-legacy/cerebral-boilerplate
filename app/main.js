0d5aec570e8db39649c9b2a93d9811ce4310f6b8
Christian Alfoni <christianalfoni@gmail.com>
2015-12-21 17:19:37 +0100

    Updated with latest versions and router

--- app/main.js
+++ app/main.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from 'cerebral-router';
 import controller from './controller';
 import {Container} from 'cerebral-react';
 
 import Home from './modules/Home';
 import homeSignals from './modules/Home/signals';
 
+controller.signal('homeRouted', [Router.redirect('/red')]);
+
 homeSignals(controller);
+
+Router(controller, {
+  '/': 'homeRouted',
+  '/:color': 'colorChanged'
+}, {
+  onlyHash: true
+});
 
 ReactDOM.render(<Container controller={controller}><Home /></Container>, document.getElementById('root'));
