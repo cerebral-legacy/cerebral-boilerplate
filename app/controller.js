df81e964cdbf8fd0d55ac621c71f15d9ce97b467
Christian Alfoni <christianalfoni@gmail.com>
2015-12-22 10:29:58 +0100

    Updated with other text

--- app/controller.js
+++ app/controller.js
@@ -2,7 +2,7 @@ import Controller from 'cerebral';
 import Model from 'cerebral-baobab';
 
 const model = Model({
-  title: 'Hello world!',
+  title: 'You can change the url too!',
   color: '#333'
 });
 
